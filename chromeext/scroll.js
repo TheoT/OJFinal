ab0f65eddee6c755f43df704d721f64542f26817
TheoT <theodore.thompson@students.olin.edu>
2013-04-26 16:40:46 -0400

    cross domain iframe solved

--- chromeext/scroll.js
+++ chromeext/scroll.js
@@ -3,14 +3,15 @@ var SERVER_PORT='3000';
 var socket = io.connect(SERVER_HOST,{port:SERVER_PORT})
 var s=0;
 var docHeight=100;
+$(function(){
+	docHeight=$(document).height;
+	s = $('body').scrollTop();
+	socket.emit('scrollSock',{scroll: s, height:docHeight});
+})
+
 $(window).scroll(function () {
 	s = $('body').scrollTop();
 	docHeight=$(document).height();
 	socket.emit('scrollSock',{scroll: s, height:docHeight})
 	console.log("doc height: "+docHeight)
 });
-
-$(function(){
-	docHeight=$(document).height;
-	socket.emit('scrollSock',{scroll: '0', height:docHeight});
-})
