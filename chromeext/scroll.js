1eb861c574b17edaaf26dfc61df849f03a499c86
abekim <abekim0607@gmail.com>
2013-04-29 17:26:38 -0400

    merging conflicts

--- chromeext/scroll.js
+++ chromeext/scroll.js
@@ -17,15 +17,15 @@ $(window).scroll(function () {
 
 //the following code captures any changes made to the textarea. We just need to have the socket emit the text
 $('#pad').bind('input propertychange', function() {
-  console.log('textarea val: ', this.value);
-  // $("#yourBtnID").hide();
+	console.log('textarea val: ', this.value);
+	// $("#yourBtnID").hide();
 
-  // if(this.value.length){
-  //   $("#yourBtnID").show();
-  // }
-  socket.emit('pirateSock', { text: '' });
+	// if(this.value.length){
+	//   $("#yourBtnID").show();
+	// }
+	socket.emit('pirateSock', { text: '' });
 
-  if(this.value.length) {
-    socket.emit('pirateSock',{ text: this.value });
-  }
+	if(this.value.length) {
+		socket.emit('pirateSock',{ text: this.value });
+	}
 });
