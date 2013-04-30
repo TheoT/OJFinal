ab91de83733e6c4a41e941959f4d57aad0dc665f
TheoT <theodore.thompson@students.olin.edu>
2013-04-30 16:46:55 -0400

    working on sync

--- public/javascripts/follow.js
+++ public/javascripts/follow.js
@@ -37,7 +37,7 @@ $(function(){
 
 function sync(){
 	syncing=true;
-	if(("#followFrame").attr('src')!=src && src!=undefined){
+	if( $("#followFrame").attr('src')!=src && src!=undefined){
 		$("#followFrame").attr('src',src);
 	}
 	$("#fakeFrame").scrollTop(pos);
