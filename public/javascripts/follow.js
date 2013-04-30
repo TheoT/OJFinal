e112028341b6028dcadf6aa34a50c6615019cc62
TheoT <theodore.thompson@students.olin.edu>
2013-04-30 16:33:55 -0400

    fix sync

--- public/javascripts/follow.js
+++ public/javascripts/follow.js
@@ -1,21 +1,26 @@
 var socket = io.connect()
 var pos;
 var src;
+var syncing=true;
 $(function(){
 	socket.on('scrollSock',function(data){
 		pos=data.scroll
-		$("#fakeFrame").scrollTop(data.scroll);
-		$("#followFrame").css("overflow","hidden");
-		console.log(data)
-		if($("#followFrame").height() != data.height){
-			$("#followFrame").height(data.height+"px")
-			console.log("doc height: "+data.height)
+		if(syncing){
+			$("#fakeFrame").scrollTop(data.scroll);
+			$("#followFrame").css("overflow","hidden");
+			console.log(data)
+			if($("#followFrame").height() != data.height){
+				$("#followFrame").height(data.height+"px")
+				console.log("doc height: "+data.height)
+			}
 		}
 	});
 
 	socket.on('pageSock',function(data){
 		src=data.page;
-		$("#followFrame").attr('src',data.page);
+		if(syncing){
+			$("#followFrame").attr('src',data.page);
+		}
 	});
 
 	socket.on('pirateSock', function (data) {
@@ -30,9 +35,13 @@ $(function(){
 });
 
 function sync(){
+	syncing=true;
 	if(("#followFrame").attr('src')!=src && src!=undefined){
 		$("#followFrame").attr('src',src);
 	}
 	$("#fakeFrame").scrollTop(pos);
 }		
 
+$("#fakeFrame").scroll(function () {
+	syncing=false;
+}
