046244e4567eacefcb463fbc891aa4a802ef7eac
dankearney <daniel.kearney@students.olin.edu>
2013-05-12 17:42:16 -0400

    made it turn off/on

--- chromeext/popup.js
+++ chromeext/popup.js
@@ -1,61 +1,72 @@
-var SERVER_HOST='ojfinal.herokuapp.com';
-// var SERVER_PORT='3000'; 
-var socket = io.connect(SERVER_HOST); //,{port:SERVER_PORT}
+// var SERVER_HOST='ojfinal.herokuapp.com';
+var SERVER_HOST='localhost';
+var SERVER_PORT='3000'; 
+var socket = io.connect(SERVER_HOST, {port:SERVER_PORT});
 
 //the following code captures any changes made to the textarea. We just need to have the socket emit the text
 $('#pad').bind('input propertychange', function() {
 	localStorage["pirateText"]=this.value;
 	console.log('textarea val: ', this.value);
 	if(this.value.length) {
-		socket.emit('pirateSock',{ text: this.value });
+		updatePirate();
 	}
 });
 
+function updatePirate() {
+	if (localStorage['leading'] == 'true') {
+		socket.emit('pirateSock', { text: localStorage['pirateText'] });
+	}
+}
 
-
-$(function () {
-	socket.on('notifySock', function (data) {
-		if (!localStorage[data.type]) {
-			localStorage[data.type] = 0;
-		}
-		else {
-			localStorage[data.type] = parseInt(localStorage[data.type]) + 1;
-		}
-		console.log('sData: ', data.type);
-		$('#' + data.type).text(localStorage[data.type]);
-	});
-})
+chrome.extension.onRequest.addListener(function(request) {                                                                                                                                                  
+    if (request.data) {                                                                                                                                                                                
+        updateText();                                                                                                                                                     
+    }
+});
 
 //grab the text and counters from the local db
 $(document).ready(function() {
+	updateText();
+	checkLeadership();
+})
 
-	$("#clearCounter").click(function () {
-		localStorage['slow'] = 0;
-		localStorage['perfect'] = 0;
-		localStorage['speed'] = 0;
-		updateText();
-	})
+function checkLeadership() {
+	var leading = (localStorage['leading'] == 'true');
+	$(leading ? "#doLead" : "#dontLead").addClass("active");
+	$(leading ? "#dontLead" : "#doLead").removeClass("active");
+}
 
-	$("#leadToggle").click(function() {
-		this.attr('id').toggleClo
-	})
-
+$("#clearCounter").click(function () {
+	localStorage['slow'] = 0;
+	localStorage['perfect'] = 0;
+	localStorage['speed'] = 0;
 	updateText();
 })
 
+//update counters
 function updateText() {
 	$("#pad").val(localStorage["pirateText"]);
 	$("#slow").text(localStorage["slow"]);
 	$("#perfect").text(localStorage["perfect"]);
 	$("#speed").text(localStorage["speed"]);
+	updatePirate();
 }
 
+//update pirate pad
 $("body").load(function() {
 	$("#pad").val() = localStorage["pirateText"];
 })
 
+//reset counters on button press
 $('#reset').on('click', function () {
 	localStorage['slow'] = 0
 	localStorage['perfect'] = 0
 	localStorage['speed'] = 0
 });
+
+//toggle leading
+$(".leadToggle").on('click', function() {
+	$(".leadToggle").toggleClass("active");
+	localStorage['leading'] = ($(this).attr('id') == "doLead");
+	updateText();
+})
--- chromeext/scroll.js
+++ chromeext/scroll.js
@@ -4,16 +4,29 @@ var SERVER_PORT='3000';
 var socket = io.connect(SERVER_HOST,{port:SERVER_PORT})
 var s=0;
 var docHeight=100;
+
 $(function(){
 	docHeight=$(document).height;
 	s = $('body').scrollTop();
-	socket.emit('scrollSock',{scroll: s, height:docHeight});
+
+	chrome.extension.sendRequest({method: "getStatus"}, function(response) {
+  		if (response.status == 'true') {
+			socket.emit('scrollSock',{scroll: s, height:docHeight});
+			console.log("doc height: "+docHeight);
+		}
+	});
 })
 
 $(window).scroll(function () {
 	s = $('body').scrollTop();
 	docHeight=$(document).height();
-	socket.emit('scrollSock',{scroll: s, height:docHeight})
-	console.log("doc height: "+docHeight)
+
+	chrome.extension.sendRequest({method: "getStatus"}, function(response) {
+  		if (response.status == 'true') {
+			socket.emit('scrollSock',{scroll: s, height:docHeight})
+			console.log("doc height: "+docHeight)
+		}
+	});
+
 
 });
