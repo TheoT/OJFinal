3e179d45cf3a69bd452975aa033f3a0d6ce4e9a7
dankearney <daniel.kearney@students.olin.edu>
2013-05-13 04:23:13 -0400

    nothing

--- chromeext/background.js
+++ chromeext/background.js
@@ -1,7 +1,7 @@
-// var SERVER_HOST='ojfinal.herokuapp.com';
-var SERVER_HOST = 'localhost';
+var SERVER_HOST='ojfinal.herokuapp.com';
+// var SERVER_HOST = 'localhost';
 var SERVER_PORT='3000';
-var socket = new io.connect(SERVER_HOST, {port: SERVER_PORT})
+var socket = new io.connect(SERVER_HOST);//, {port: SERVER_PORT})
 
 localStorage['leading'] = 'false'; //set default leadership false
 
